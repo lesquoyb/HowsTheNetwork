d9afd186bb61759dac86b4928819ad46baeff614
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2024-05-19 00:31:54 +0700

    First attempt at using curses

--- console_client.py
+++ console_client.py
@@ -1,4 +1,7 @@
+import asyncio
 from datetime import datetime
+import curses
+from typing import Optional
 
 from HowsTheNetwork.bandwidth_statistics import BandwidthStatistics
 from HowsTheNetwork.client import Client
@@ -7,22 +10,72 @@ from HowsTheNetwork.connection_statistics import ConnectionStatistics
 
 class ConsoleClient(Client):
 
+    CONNECTION_SCREEN_HEIGHT = 5
+    BANDWIDTH_SCREEN_HEIGHT = 5
+
+    def __init__(self, connection, bandwidth):
+
+        self.current_connection_statistics: Optional[ConnectionStatistics] = None
+        self.current_bandwidth_statistics: Optional[BandwidthStatistics] = None
+
+        if connection or bandwidth:
+            self.whole_screen = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            self.bandwidth_start_line = 0
+            if connection:
+                self.bandwidth_start_line += self.CONNECTION_SCREEN_HEIGHT + 1
+
+            self.start_user_input_thread()
+
+    def start_user_input_thread(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.user_input_thread())
+
+    async def user_input_thread(self):
+        # sets negative timeout so getch becomes blocking
+        self.whole_screen.timeout(-1)
+        self.whole_screen.nodelay(True)
+        while True:
+            h = self.whole_screen.getch()
+            if h == ord('q'):
+                self.closing()
+            await asyncio.sleep(2)
+
+    def write_line(self, line: int, string: str):
+        self.whole_screen.addstr(line, 0, string)
+        self.whole_screen.clrtoeol() # overwrite the old stuff
+
+    def update_screen(self):
+        self.whole_screen.clear()
+        if self.current_connection_statistics:
+            self.write_line(0, f"current state: {'connected' if self.current_connection_statistics.currently_connected else 'not connected'}")
+            self.write_line(1, f"longest disconnection: {self.current_connection_statistics.longest_duration // 60}m{self.current_connection_statistics.longest_duration % 60:.0f}s "
+                               f"at time {datetime.fromtimestamp(self.current_connection_statistics.start_longest) if self.current_connection_statistics.start_longest > 0 else 0}")
+            self.write_line(2, f"average disconnection duration: {self.current_connection_statistics.average_duration:.0f}s")
+            self.write_line(3, f"total number of disconnection: {self.current_connection_statistics.nb_disconnection}")
+            self.write_line(4, f"average number of disconnection per hour: {self.current_connection_statistics.average_nb_disc_hour:.2f}")
+        if self.current_bandwidth_statistics:
+            self.write_line(self.bandwidth_start_line, f"Real network use since last update: {self.current_bandwidth_statistics.current_network_use:.0f}Kbits")
+            self.write_line(self.bandwidth_start_line + 1, f"Real network speed since last update: {self.current_bandwidth_statistics.current_network_speed:.0f}Kbits/second")
+            self.write_line(self.bandwidth_start_line + 2, f"Average network use since the beginning: {self.current_bandwidth_statistics.average_network_use:.0f}Kbits/second")
+            self.write_line(self.bandwidth_start_line + 3, f"Total network use: {self.current_bandwidth_statistics.total_use:.0f}Kbits")
+            self.write_line(self.bandwidth_start_line + 4, f"Total monitoring duration: {self.current_bandwidth_statistics.total_duration//60}m{self.current_bandwidth_statistics.total_duration % 60}s")
+
+        self.whole_screen.refresh()
 
     def update_internet_statistics(self, stats: ConnectionStatistics):
-        print("")
-        print(f"current state: {'connected' if stats.currently_connected else 'not connected'}")
-        print(f"longest disconnection: {stats.longest_duration // 60}m{stats.longest_duration % 60:.0f}s "
-              f"at time {datetime.fromtimestamp(stats.start_longest) if stats.start_longest > 0 else 0}")
-        print(f"average disconnection duration: {stats.average_duration:.0f}s")
-        print(f"total number of disconnection: {stats.nb_disconnection}")
-        print(f"average number of disconnection per hour: {stats.average_nb_disc_hour:.2f}")
+        self.current_connection_statistics = stats
+        self.update_screen()
 
     def update_bandwidth_statistics(self, stats: BandwidthStatistics):
-        print("")
-        print(f"Real network use since last update: {stats.current_network_use:.0f}Kbits")
-        print(f"Real network speed since last update: {stats.current_network_speed:.0f}Kbits/second")
-        print(f"Average network use since the beginning: {stats.average_network_use:.0f}Kbits/second")
-        print(f"Total network use: {stats.total_use:.0f}Kbits")
-        print(f"Total monitoring duration: {stats.total_duration//60}m{stats.total_duration % 60}s")
+        self.current_bandwidth_statistics = stats
+        self.update_screen()
 
-
+    def closing(self):
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+        # we need to stop asyncio event loop to stop the program
+        loop = asyncio.get_event_loop()
+        loop.stop()
